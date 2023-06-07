6082462fbd8e225f20d250cdaf5b61614a00bbf6
Hasan <hasanjonsadullayev97@gmail.com>
2023-06-07 12:07:10 +0500

    requir udate to import

--- src/modules/payment.js
+++ src/modules/payment.js
@@ -1,4 +1,4 @@
-const { Schema, model } = require('mongoose');
+import { Schema, model } from 'mongoose';
 
 const paymentSchema = new Schema({
 	cartNumber: { type: String, limit: 16, required: true },
